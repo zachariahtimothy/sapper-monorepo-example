0b82b5927f8c4f4451e4a962eec4d23820910315
Zach Curtis <zachariahtimothy@gmail.com>
2019-04-23 09:08:00 -0600

    Added initial working solution

---
+++ clientApps/webapp-example/__sapper__/dev/client/chunk.06b6fc4b.js
@@ -0,0 +1,404 @@
+function noop() {}
+
+function assign(tar, src) {
+	for (const k in src) tar[k] = src[k];
+	return tar;
+}
+
+function add_location(element, file, line, column, char) {
+	element.__svelte_meta = {
+		loc: { file, line, column, char }
+	};
+}
+
+function run(fn) {
+	return fn();
+}
+
+function blank_object() {
+	return Object.create(null);
+}
+
+function run_all(fns) {
+	fns.forEach(run);
+}
+
+function is_function(thing) {
+	return typeof thing === 'function';
+}
+
+function safe_not_equal(a, b) {
+	return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+}
+
+function create_slot(definition, ctx, fn) {
+	if (definition) {
+		const slot_ctx = get_slot_context(definition, ctx, fn);
+		return definition[0](slot_ctx);
+	}
+}
+
+function get_slot_context(definition, ctx, fn) {
+	return definition[1]
+		? assign({}, assign(ctx.$$scope.ctx, definition[1](fn ? fn(ctx) : {})))
+		: ctx.$$scope.ctx;
+}
+
+function get_slot_changes(definition, ctx, changed, fn) {
+	return definition[1]
+		? assign({}, assign(ctx.$$scope.changed || {}, definition[1](fn ? fn(changed) : {})))
+		: ctx.$$scope.changed || {};
+}
+
+function append(target, node) {
+	target.appendChild(node);
+}
+
+function insert(target, node, anchor) {
+	target.insertBefore(node, anchor);
+}
+
+function detach(node) {
+	node.parentNode.removeChild(node);
+}
+
+function destroy_each(iterations, detaching) {
+	for (let i = 0; i < iterations.length; i += 1) {
+		if (iterations[i]) iterations[i].d(detaching);
+	}
+}
+
+function element(name) {
+	return document.createElement(name);
+}
+
+function svg_element(name) {
+	return document.createElementNS('http://www.w3.org/2000/svg', name);
+}
+
+function text(data) {
+	return document.createTextNode(data);
+}
+
+function space() {
+	return text(' ');
+}
+
+function empty() {
+	return text('');
+}
+
+function children(element) {
+	return Array.from(element.childNodes);
+}
+
+function claim_element(nodes, name, attributes, svg) {
+	for (let i = 0; i < nodes.length; i += 1) {
+		const node = nodes[i];
+		if (node.nodeName === name) {
+			for (let j = 0; j < node.attributes.length; j += 1) {
+				const attribute = node.attributes[j];
+				if (!attributes[attribute.name]) node.removeAttribute(attribute.name);
+			}
+			return nodes.splice(i, 1)[0]; // TODO strip unwanted attributes
+		}
+	}
+
+	return svg ? svg_element(name) : element(name);
+}
+
+function claim_text(nodes, data) {
+	for (let i = 0; i < nodes.length; i += 1) {
+		const node = nodes[i];
+		if (node.nodeType === 3) {
+			node.data = data;
+			return nodes.splice(i, 1)[0];
+		}
+	}
+
+	return text(data);
+}
+
+function set_data(text, data) {
+	data = '' + data;
+	if (text.data !== data) text.data = data;
+}
+
+let current_component;
+
+function set_current_component(component) {
+	current_component = component;
+}
+
+function get_current_component() {
+	if (!current_component) throw new Error(`Function called outside component initialization`);
+	return current_component;
+}
+
+function setContext(key, context) {
+	get_current_component().$$.context.set(key, context);
+}
+
+const dirty_components = [];
+
+let update_promise;
+const binding_callbacks = [];
+const render_callbacks = [];
+const flush_callbacks = [];
+
+function schedule_update() {
+	if (!update_promise) {
+		update_promise = Promise.resolve();
+		update_promise.then(flush);
+	}
+}
+
+function add_render_callback(fn) {
+	render_callbacks.push(fn);
+}
+
+function flush() {
+	const seen_callbacks = new Set();
+
+	do {
+		// first, call beforeUpdate functions
+		// and update components
+		while (dirty_components.length) {
+			const component = dirty_components.shift();
+			set_current_component(component);
+			update(component.$$);
+		}
+
+		while (binding_callbacks.length) binding_callbacks.shift()();
+
+		// then, once components are updated, call
+		// afterUpdate functions. This may cause
+		// subsequent updates...
+		while (render_callbacks.length) {
+			const callback = render_callbacks.pop();
+			if (!seen_callbacks.has(callback)) {
+				callback();
+
+				// ...so guard against infinite loops
+				seen_callbacks.add(callback);
+			}
+		}
+	} while (dirty_components.length);
+
+	while (flush_callbacks.length) {
+		flush_callbacks.pop()();
+	}
+
+	update_promise = null;
+}
+
+function update($$) {
+	if ($$.fragment) {
+		$$.update($$.dirty);
+		run_all($$.before_render);
+		$$.fragment.p($$.dirty, $$.ctx);
+		$$.dirty = null;
+
+		$$.after_render.forEach(add_render_callback);
+	}
+}
+
+let outros;
+
+function group_outros() {
+	outros = {
+		remaining: 0,
+		callbacks: []
+	};
+}
+
+function check_outros() {
+	if (!outros.remaining) {
+		run_all(outros.callbacks);
+	}
+}
+
+function on_outro(callback) {
+	outros.callbacks.push(callback);
+}
+
+function get_spread_update(levels, updates) {
+	const update = {};
+
+	const to_null_out = {};
+	const accounted_for = {};
+
+	let i = levels.length;
+	while (i--) {
+		const o = levels[i];
+		const n = updates[i];
+
+		if (n) {
+			for (const key in o) {
+				if (!(key in n)) to_null_out[key] = 1;
+			}
+
+			for (const key in n) {
+				if (!accounted_for[key]) {
+					update[key] = n[key];
+					accounted_for[key] = 1;
+				}
+			}
+
+			levels[i] = n;
+		} else {
+			for (const key in o) {
+				accounted_for[key] = 1;
+			}
+		}
+	}
+
+	for (const key in to_null_out) {
+		if (!(key in update)) update[key] = undefined;
+	}
+
+	return update;
+}
+
+function mount_component(component, target, anchor) {
+	const { fragment, on_mount, on_destroy, after_render } = component.$$;
+
+	fragment.m(target, anchor);
+
+	// onMount happens after the initial afterUpdate. Because
+	// afterUpdate callbacks happen in reverse order (inner first)
+	// we schedule onMount callbacks before afterUpdate callbacks
+	add_render_callback(() => {
+		const new_on_destroy = on_mount.map(run).filter(is_function);
+		if (on_destroy) {
+			on_destroy.push(...new_on_destroy);
+		} else {
+			// Edge case - component was destroyed immediately,
+			// most likely as a result of a binding initialising
+			run_all(new_on_destroy);
+		}
+		component.$$.on_mount = [];
+	});
+
+	after_render.forEach(add_render_callback);
+}
+
+function destroy(component, detaching) {
+	if (component.$$) {
+		run_all(component.$$.on_destroy);
+		component.$$.fragment.d(detaching);
+
+		// TODO null out other refs, including component.$$ (but need to
+		// preserve final state?)
+		component.$$.on_destroy = component.$$.fragment = null;
+		component.$$.ctx = {};
+	}
+}
+
+function make_dirty(component, key) {
+	if (!component.$$.dirty) {
+		dirty_components.push(component);
+		schedule_update();
+		component.$$.dirty = {};
+	}
+	component.$$.dirty[key] = true;
+}
+
+function init(component, options, instance, create_fragment, not_equal$$1, prop_names) {
+	const parent_component = current_component;
+	set_current_component(component);
+
+	const props = options.props || {};
+
+	const $$ = component.$$ = {
+		fragment: null,
+		ctx: null,
+
+		// state
+		props: prop_names,
+		update: noop,
+		not_equal: not_equal$$1,
+		bound: blank_object(),
+
+		// lifecycle
+		on_mount: [],
+		on_destroy: [],
+		before_render: [],
+		after_render: [],
+		context: new Map(parent_component ? parent_component.$$.context : []),
+
+		// everything else
+		callbacks: blank_object(),
+		dirty: null
+	};
+
+	let ready = false;
+
+	$$.ctx = instance
+		? instance(component, props, (key, value) => {
+			if ($$.ctx && not_equal$$1($$.ctx[key], $$.ctx[key] = value)) {
+				if ($$.bound[key]) $$.bound[key](value);
+				if (ready) make_dirty(component, key);
+			}
+		})
+		: props;
+
+	$$.update();
+	ready = true;
+	run_all($$.before_render);
+	$$.fragment = create_fragment($$.ctx);
+
+	if (options.target) {
+		if (options.hydrate) {
+			$$.fragment.l(children(options.target));
+		} else {
+			$$.fragment.c();
+		}
+
+		if (options.intro && component.$$.fragment.i) component.$$.fragment.i();
+		mount_component(component, options.target, options.anchor);
+		flush();
+	}
+
+	set_current_component(parent_component);
+}
+
+class SvelteComponent {
+	$destroy() {
+		destroy(this, true);
+		this.$destroy = noop;
+	}
+
+	$on(type, callback) {
+		const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+		callbacks.push(callback);
+
+		return () => {
+			const index = callbacks.indexOf(callback);
+			if (index !== -1) callbacks.splice(index, 1);
+		};
+	}
+
+	$set() {
+		// overridden by instance, if it has props
+	}
+}
+
+class SvelteComponentDev extends SvelteComponent {
+	constructor(options) {
+		if (!options || (!options.target && !options.$$inline)) {
+			throw new Error(`'target' is a required option`);
+		}
+
+		super();
+	}
+
+	$destroy() {
+		super.$destroy();
+		this.$destroy = () => {
+			console.warn(`Component was already destroyed`); // eslint-disable-line no-console
+		};
+	}
+}
+
+export { destroy_each as A, noop as a, safe_not_equal as b, SvelteComponentDev as c, init as d, element as e, text as f, space as g, claim_element as h, children as i, claim_text as j, detach as k, add_location as l, insert as m, append as n, create_slot as o, mount_component as p, get_slot_changes as q, get_slot_context as r, set_data as s, empty as t, assign as u, get_spread_update as v, setContext as w, on_outro as x, check_outros as y, group_outros as z };
